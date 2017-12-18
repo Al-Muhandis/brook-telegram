677a396737b2de2ab40afe1137cd421cbc6f7c46
Al-Muhandis <mail@Renat.Su>
2017-12-18 22:33:17 +0400

    added: i18n support, etc

--- brooktelegramaction.pas
+++ brooktelegramaction.pas
@@ -10,6 +10,10 @@ uses
 
 type
 
+  TUserStatus = (usAdmin, usModerator, usDefault, usBanned);
+
+  TWebhookBot = class;
+
   { TWebhookAction }
 
   TWebhookAction = class(TBrookAction)
@@ -21,11 +25,12 @@ type
     FStartText: String;
     FStatLogger: TtgStatLog;
     FToken: String;
-    FBot: TTelegramSender;
-    FUserPermissions: TStringList; // namevalue pairs UserID=Character
+    FBot: TWebhookBot;
     procedure BotStartHandler(ASender: TObject; const ACommand: String;
       AMessage: TTelegramMessageObj);
     procedure BotHelpHandler(ASender: TObject; const ACommand: String;
+      AMessage: TTelegramMessageObj);
+    procedure BotSetCommandReply(ASender: TObject; const ACommand: String;
       AMessage: TTelegramMessageObj);
     procedure BotStatHandler(ASender: TObject; const ACommand: String;
       AMessage: TTelegramMessageObj);
@@ -54,6 +59,8 @@ type
     procedure EditOrSendMessage(const AMessage: String; AParseMode: TParseMode = pmDefault;
       ReplyMarkup: TReplyMarkup = nil; TryEdit: Boolean = False);
     function IsSimpleUser: Boolean;
+    function IsBanned: Boolean;
+    procedure SaveFeedback(AFrom: TTelegramUserObj; AMessage: String); virtual; abstract;
   public
     constructor Create; override;
     destructor Destroy; override;
@@ -61,37 +68,100 @@ type
     property Token: String read FToken write SetToken;
     property OnCallbackQuery: TCallbackEvent read FOnCallbackQuery write SetOnCallbackQuery;
     property OnUpdateMessage: TMessageEvent read FOnUpdateMessage write SetOnUpdateMessage;
-    property UserPermissions: TStringList read FUserPermissions write FUserPermissions;
     property StartText: String read FStartText write SetStartText; // Text for /start command reply
     property HelpText: String read FHelpText write SetHelpText;  // Text for /help command reply
     property StatLogger: TtgStatLog read FStatLogger write SetStatLogger;
     property Logger: TEventLog read FLogger write SetLogger;
-    property Bot: TTelegramSender read FBot;
+    property Bot: TWebhookBot read FBot;
   end;
 
   { TWebhookBot }
   TWebhookBot = class(TTelegramSender)
   private
     FBrookAction: TWebhookAction;
+    FFeedbackText: String;
+    FFeedbackThanks: String;
+    FUserPermissions: TStringList;
+    function GetUserStatus(ID: Int64): TUserStatus;
     procedure SetBrookAction(AValue: TWebhookAction);
+    procedure SetUserStatus(ID: Int64; AValue: TUserStatus);
+    procedure TlgrmFeedback(ASender: TObject; const ACommand: String;
+      AMessage: TTelegramMessageObj);
+    procedure LangTranslate(const ALang: String);
   protected
-    procedure DoReceiveMessageUpdate; override;
-    procedure DoReceiveCallbackQuery; override;
+    procedure DoReceiveMessageUpdate(AMessage: TTelegramMessageObj); override;
+    procedure DoReceiveCallbackQuery(ACallback: TCallbackQueryObj); override;
     procedure DebugMessage(const Msg: String); override;
     procedure InfoMessage(const Msg: String); override;
     procedure ErrorMessage(const Msg: String); override;
     property BrookAction: TWebhookAction read FBrookAction write SetBrookAction;
+    function IsBanned(ChatID: Int64): Boolean; override;       
+    function IsSimpleUser(ChatID: Int64): Boolean; override;
+    procedure SetLanguage(const ALang: String); override;
   public
     constructor Create(const AToken: String; AWebhookAction: TWebhookAction);
+    destructor Destroy; override;
+    procedure LoadUserStatusValues(AStrings: TStrings);
+    property FeedbackText: String read FFeedbackText write FFeedbackText;
+    property FeedbackThanks: String read FFeedbackThanks write FFeedbackThanks;
+    property UserStatus [ID: Int64]: TUserStatus read GetUserStatus write SetUserStatus;
   end;
 
 
 implementation
 
-uses jsonparser, BrookHttpConsts, strutils, BrookApplication, jsonscanner;
+uses jsonparser, BrookHttpConsts, strutils, BrookApplication, jsonscanner, Translations;
+
+// Please use i18n for localization *** Пожалуйста, используйте i18n для локализации
+resourcestring
+  str_FeedbackThanks='Thanks! %s Your message will be considered!'; // Спасибо, %s! Ваш сообщение будет обязательно рассмотрено!;
+  str_FeedbackText='Send us a wish, recommendation or bug report';  // Отправьте разработчику бота пожелание, рекомендацию, информацию об ошибке
+  str_TxtRplyIsScsChngd='Text reply for command is succesfully changed!'; // Текст сообщения для команды успешно изменен
+  str_BtApIsClsd='Bot app is closed';  // Приложение закрыто
+  str_SttstcsNtFnd='Statistics for this date not found'; // Статистика не найдено за указанный день
+  str_ErrFldTLdSttstcsFl='Error: failed to load statistics file';
+  str_EntrDtInFrmt='Please enter the date in format: dd-mm-yyyy';
+  str_SttstcsFr='Statistics for ';
+  str_SlctSttstcs_line1='Select statistics by pressing the button. In addition, the available commands:';
+  str_SlctSttstcs_line2='/stat <i>day</i> - the last records for a specified <i>date</i>, ';
+  str_SlctSttstcs_line3= '/statf <i>date</i> - statistics file for the <i>date</i>,';
+  str_SlctSttstcs_line4='where <i>date</i> is <i>today</i> or <i>yesterday</i> or in format <i>dd-mm-yyyy</i>';
+  str_Today='Today';
+  str_Yesterday='Yesterday';
+
 
 const
   StatDateFormat = 'dd-mm-yyyy';
+  UserStatusChars: array [TUserStatus] of AnsiChar = ('a', 'm', 'b', '_');
+  cmd_Start = '/start';
+  cmd_Help = '/help';
+  cmd_Stat = '/stat';
+  cmd_StatF = '/statf';
+  cmd_Terminate = '/terminate';
+  cmd_Feedback = '/feedback';
+  cmd_SetStart = '/setstart';
+  cmd_SetHelp = '/sethelp';
+  s_Today = 'today';
+  s_Yesterday = 'yesterday';
+  s_GetStat='GetStat';
+  s_File='File';
+
+function AnsiCharToUserStatus(const StatusChar: String): TUserStatus;
+var
+  a: AnsiChar;
+begin
+  if StatusChar=EmptyStr then
+    a:=UserStatusChars[usDefault]
+  else
+    a:=PAnsiChar(StatusChar)[0];
+  case a of
+    'a': Result:=usAdmin;
+    'm': Result:=usModerator;
+    'b': Result:=usBanned;
+  else
+    Result:=usDefault;
+  end;
+end;
 
 { TWebhookBot }
 
@@ -101,23 +171,90 @@ begin
   FBrookAction:=AValue;
 end;
 
+function TWebhookBot.GetUserStatus(ID: Int64): TUserStatus;
+begin
+  Result:=AnsiCharToUserStatus(FUserPermissions.Values[IntToStr(ID)]);
+end;
+
+procedure TWebhookBot.SetUserStatus(ID: Int64; AValue: TUserStatus);
+var
+  i: Integer;
+begin
+  if AValue=usDefault then
+  begin
+    i:= FUserPermissions.IndexOf(IntToStr(ID));
+    if i >-1 then
+      FUserPermissions.Delete(i)
+    else
+      Exit;
+  end;
+  FUserPermissions.Values[IntToStr(ID)]:=UserStatusChars[AValue];
+end;
+
+procedure TWebhookBot.TlgrmFeedback(ASender: TObject; const ACommand: String;
+  AMessage: TTelegramMessageObj);
+var
+  ReplyMarkup: TReplyMarkup;
+begin
+  ReplyMarkup:=TReplyMarkup.Create;
+  try
+    ReplyMarkup.ForceReply:=True;
+    sendMessage(FFeedbackText, pmMarkdown, True, ReplyMarkup);
+  finally
+    ReplyMarkup.Free;
+  end;
+end;
+
+procedure TWebhookBot.LangTranslate(const ALang: String);
+var
+  L, F: String;
+begin
+  if Length(ALang)>2 then
+    L:=LeftStr(ALang, 2)
+  else
+    L:=ALang;
+  F:='languages'+PathDelim+ApplicationName+'.%s.po';
+  TranslateResourceStrings(F, L, '');
+end;
+
 constructor TWebhookBot.Create(const AToken: String;
   AWebhookAction: TWebhookAction);
 begin
   inherited Create(AToken);
   FBrookAction:=AWebhookAction;
+  FUserPermissions:=TStringList.Create;
+  FUserPermissions.Sorted:=True;
+  FUserPermissions.Duplicates:=dupIgnore;
+  FFeedbackThanks:=str_FeedbackThanks;
+  FFeedbackText:=str_FeedbackText;
+  CommandHandlers[cmd_Feedback]:=@TlgrmFeedback;
 end;
 
-procedure TWebhookBot.DoReceiveMessageUpdate;
+destructor TWebhookBot.Destroy;
 begin
-  inherited DoReceiveMessageUpdate;
-  FBrookAction.BotMessageHandler(CurrentUpdate.Message);
+  FUserPermissions.Free;
+  inherited Destroy;
 end;
 
-procedure TWebhookBot.DoReceiveCallbackQuery;
+procedure TWebhookBot.DoReceiveMessageUpdate(AMessage: TTelegramMessageObj);
 begin
-  inherited DoReceiveCallbackQuery;
-  FBrookAction.BotCallbackQuery(CurrentUpdate.CallbackQuery);
+  inherited DoReceiveMessageUpdate(AMessage);
+  if Assigned(AMessage.ReplyToMessage) then
+  begin
+    if Assigned(AMessage.ReplyToMessage.From) then
+      {if SameText(AMessage.ReplyToMessage.From.Username, %BotUserName%) then}
+        sendMessage(Format(FFeedbackThanks, [AMessage.From.First_name]));
+    With AMessage do
+      FBrookAction.SaveFeedback(From, Text);
+    Exit;
+  end;
+  FBrookAction.BotMessageHandler(AMessage);
+end;
+
+procedure TWebhookBot.DoReceiveCallbackQuery(ACallback: TCallbackQueryObj);
+begin
+  inherited DoReceiveCallbackQuery(ACallback);
+  FBrookAction.BotCallbackQuery(ACallback);
 end;
 
 procedure TWebhookBot.DebugMessage(const Msg: String);
@@ -136,6 +273,28 @@ procedure TWebhookBot.ErrorMessage(const Msg: String);
 begin
   inherited ErrorMessage(Msg);
   FBrookAction.LogMessage(Self, etError, Msg);
+end;
+
+function TWebhookBot.IsBanned(ChatID: Int64): Boolean;
+begin
+  Result:=FUserPermissions.Values[IntToStr(ChatID)]='b'
+end;
+
+function TWebhookBot.IsSimpleUser(ChatID: Int64): Boolean;
+begin
+  Result:=(FUserPermissions.Values[IntToStr(ChatID)]<>'a') and
+    (FUserPermissions.Values[IntToStr(ChatID)]<>'m');
+end;
+
+procedure TWebhookBot.SetLanguage(const ALang: String);
+begin
+  inherited SetLanguage(ALang);
+  LangTranslate(ALang);
+end;
+
+procedure TWebhookBot.LoadUserStatusValues(AStrings: TStrings);
+begin
+  FUserPermissions.AddStrings(AStrings);
 end;
 
   { TWebhookAction }
@@ -176,7 +335,7 @@ procedure TWebhookAction.BotStartHandler(ASender: TObject;
   const ACommand: String; AMessage: TTelegramMessageObj);
 begin
   Bot.RequestWhenAnswer:=True;
-  Bot.sendMessage(FStartText);
+  Bot.sendMessage(FStartText, pmMarkdown);
 end;
 
 procedure TWebhookAction.BotHelpHandler(ASender: TObject;
@@ -184,6 +343,22 @@ procedure TWebhookAction.BotHelpHandler(ASender: TObject;
 begin
   Bot.RequestWhenAnswer:=True;
   Bot.sendMessage(FHelpText);
+end;
+{ Caution! You must save this values in db or in configuration file! }
+procedure TWebhookAction.BotSetCommandReply(ASender: TObject;
+  const ACommand: String; AMessage: TTelegramMessageObj);
+var
+  S: String;
+begin
+  if IsSimpleUser then
+    Exit;
+  S:=Trim(RightStr(AMessage.Text, Length(AMessage.Text)-Length(ACommand)));
+  if ACommand=cmd_SetStart then
+    StartText:=S
+  else
+    if ACommand=cmd_SetHelp then
+      HelpText:=S;
+  Bot.sendMessage(str_TxtRplyIsScsChngd);
 end;
 
 procedure TWebhookAction.BotStatHandler(ASender: TObject;
@@ -222,7 +397,7 @@ begin
   if IsSimpleUser then
     Exit;
   FBot.RequestWhenAnswer:=True;
-  FBot.sendMessage('Bot app is closed');
+  FBot.sendMessage(str_BtApIsClsd);
   BrookApp.Terminate;
 end;
 
@@ -240,19 +415,21 @@ end;
 
 function TWebhookAction.IsSimpleUser: Boolean;
 begin
-  if Assigned(FBot.CurrentUser) then
-    Result:=FUserPermissions.Values[IntToStr(FBot.CurrentUser.ID)]=EmptyStr
-  else
-    Result:=True;
+  Result:=FBot.CurrentIsSimpleUser;
+end;
+
+function TWebhookAction.IsBanned: Boolean;
+begin
+  Result:=Bot.CurrentIsBanned;
 end;
 
 procedure TWebhookAction.BotCallbackQuery(ACallback: TCallbackQueryObj);
 begin
   if not IsSimpleUser then
   begin
-    if AnsiStartsStr('GetStat ', ACallback.Data) then
+    if AnsiStartsStr(s_GetStat+' ', ACallback.Data) then
       DoCallbackQueryStat(ACallback);
-    if AnsiStartsStr('GetStatFile ', ACallback.Data) then
+    if AnsiStartsStr(s_GetStat+s_File+' ', ACallback.Data) then
       DoCallbackQueryStat(ACallback, True);
   end;
   StatLog(ACallback.Data, utCallbackQuery);
@@ -307,9 +484,9 @@ begin
             EditOrSendMessage(Msg, pmHTML, ReplyMarkup, True);
           end
           else
-            EditOrSendMessage('Statistics for this date not found', pmDefault, ReplyMarkup, True);
+            EditOrSendMessage(str_SttstcsNtFnd, pmDefault, ReplyMarkup, True);
         except
-          EditOrSendMessage('Error: failed to load statistics file', pmDefault, ReplyMarkup);
+          EditOrSendMessage(str_ErrFldTLdSttstcsFl, pmDefault, ReplyMarkup);
         end;
       finally
         StatFile.Free;
@@ -328,16 +505,16 @@ begin
   if not Assigned(FStatLogger) then
     Exit;
   S:=Trim(SDate);
-  if (S='today') or (S=EmptyStr) then
+  if (S=s_Today) or (S=EmptyStr) then
     FDate:=Date
   else
-    if S='yesterday' then
+    if S=s_Yesterday then
       FDate:=Date-1
     else
       if not TryStrToDate(S, FDate, StatDateFormat) then
       begin
         FBot.RequestWhenAnswer:=True;
-        FBot.sendMessage('Please enter the date in format: dd-mm-yyyy');
+        FBot.sendMessage(str_EntrDtInFrmt);
         Exit;
       end;
   DoGetStat(FDate, SendFile);
@@ -353,12 +530,12 @@ begin
   if FileExists(AFileName) then
   begin
     FBot.RequestWhenAnswer:=False;
-    FBot.sendDocumentByFileName(Bot.CurrentChatId, AFileName, 'Statistics for '+DateToStr(ADate));
+    FBot.sendDocumentByFileName(Bot.CurrentChatId, AFileName, str_SttstcsFr+DateToStr(ADate));
   end
   else
   begin
     FBot.RequestWhenAnswer:=True;
-    EditOrSendMessage('Statistics for this date not found', pmDefault, AReplyMarkup, True);
+    EditOrSendMessage(str_SttstcsNtFnd, pmDefault, AReplyMarkup, True);
   end;
 end;
 
@@ -370,12 +547,8 @@ begin
   try
     ReplyMarkup.InlineKeyBoard:=CreateInlineKeyboardStat(SendFile);
     FBot.RequestWhenAnswer:=True;
-    FBot.sendMessage(
-      'Select statistics by pressing the button. In addition, the available commands:'+
-      LineEnding+'/stat <i>day</i> - the last records for a specified <i>date</i>, '+
-      '/statf <i>date</i> - statistics file for the <i>date</i>,'+LineEnding+
-      'where <i>date</i> is <i>today</i> or <i>yesterday</i> or in format <i>dd-mm-yyyy</i>',
-      pmHTML, True, ReplyMarkup);
+    FBot.sendMessage(str_SlctSttstcs_line1+LineEnding+str_SlctSttstcs_line2+LineEnding+
+      str_SlctSttstcs_line3+LineEnding+str_SlctSttstcs_line4, pmHTML, True, ReplyMarkup);
   finally
     ReplyMarkup.Free;
   end;
@@ -408,7 +581,7 @@ it is desirable not to create a new message and edit the message from which the 
 procedure TWebhookAction.EditOrSendMessage(const AMessage: String;
   AParseMode: TParseMode; ReplyMarkup: TReplyMarkup; TryEdit: Boolean);
 begin
-  if not TryEdit then
+  if not TryEdit or (Bot.CurrentUpdate.UpdateType<>utCallbackQuery) then
     Bot.sendMessage(AMessage, AParseMode, True, ReplyMarkup)
   else
     Bot.editMessageText(AMessage, AParseMode, True, ReplyMarkup);
@@ -424,8 +597,8 @@ begin
   else
     FileApp:='';
   btns:=TInlineKeyboardButtons.Create;
-  btns.AddButtons(['Today 🔃', 'GetStat'+FileApp+' today',
-    'Yesterday', 'GetStat'+FileApp+' yesterday']);
+  btns.AddButtons([str_Today+' 🔃', s_GetStat+FileApp+' '+s_Today,
+    str_Yesterday, s_GetStat+FileApp+' '+s_Yesterday]);
   Result:=TJSONArray.Create;
   Result.Add(btns);
 end;
@@ -433,24 +606,22 @@ end;
 constructor TWebhookAction.Create;
 begin
   inherited Create;
-  FUserPermissions:=TStringList.Create;
-  FUserPermissions.Sorted:=True;
-  FUserPermissions.Duplicates:=dupIgnore;
   FStatLogger:=TtgStatLog.Create(nil);
   FStatLogger.Active:=False;
   FBot:=TWebhookBot.Create(FToken, Self);
-  FBot.CommandHandlers['/start']:=@BotStartHandler;
-  FBot.CommandHandlers['/help']:=@BotHelpHandler;
-  FBot.CommandHandlers['/stat']:=@BotStatHandler;
-  FBot.CommandHandlers['/statf']:=@BotStatFHandler;
-
+  FBot.CommandHandlers[cmd_Start]:=@BotStartHandler;
+  FBot.CommandHandlers[cmd_Help]:=@BotHelpHandler;
+  FBot.CommandHandlers[cmd_Stat]:=@BotStatHandler;
+  FBot.CommandHandlers[cmd_StatF]:=@BotStatFHandler;
+  FBot.CommandHandlers[cmd_Terminate]:=@BotTerminateHandler;
+  FBot.CommandHandlers[cmd_SetStart]:=@BotSetCommandReply;
+  FBot.CommandHandlers[cmd_SetHelp]:=@BotSetCommandReply;
 end;
 
 destructor TWebhookAction.Destroy;
 begin
   FBot.Free;
   FStatLogger.Free;
-  FUserPermissions.Free;
   inherited Destroy;
 end;
 
